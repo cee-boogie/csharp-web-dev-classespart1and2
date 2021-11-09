f83a69d1d0bebc888f203035530f04dd3b060ff4
Christine <cboegemann@gmail.com>
2021-11-08 20:15:12 -0500

    completed exercise 4.7

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace SchoolPractice
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public List<string> Students { get; set; }
+        public Dictionary<int, string> StudentsById = new Dictionary<int, string>
+        {
+
+        };
+
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,70 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+           Student christine = new Student
+            {
+                Name = "Christine",
+                StudentId = 2,
+                NumberOfCredits = 1,
+                Gpa = 4.0
+            };
+
+            Student allison = new Student
+            {
+                Name = "Allison",
+                StudentId = 4,
+                NumberOfCredits = 1,
+                Gpa = 4.0
+            };
+
+            Student joe = new Student
+            {
+                Name = "Joe",
+                StudentId = 3,
+                NumberOfCredits = 1,
+                Gpa = 3.5
+            };
+
+            Teacher missDavid = new Teacher
+            {
+                FirstName = "Miss",
+                LastName = "David",
+                Subject = "GT",
+                YearsTeaching = 20
+            };
+
+            Course gt = new Course
+            {
+                Name = "GT",
+                Teacher = "Miss David",
+                StudentsById =
+                {
+                    {christine.StudentId, christine.Name },
+                    {allison.StudentId, allison.Name },
+                    {joe.StudentId, joe.Name }
+                }
+
+            };
+
+
+
+            string roster = string.Join(", ", gt.StudentsById);
+
+            Console.WriteLine($"Student Name: {christine.Name} \n" +
+                $"Student ID: {christine.StudentId} \n" +
+                $"Credits: {christine.NumberOfCredits} \n" +
+                $"GPA: {christine.Gpa} \n");
+
+
+            Console.WriteLine($"Course Name: {gt.Name} \n" +
+                $"Teacher: {gt.Teacher} \n" +
+                $"Students: {roster} \n");
+
+            Console.WriteLine($"Teacher Name: {missDavid.FirstName + " " + missDavid.LastName} \n" +
+                $"Subject: {missDavid.Subject} \n" +
+                $"Years Teaching: {missDavid.YearsTeaching}");
+
         }
     }
 }
